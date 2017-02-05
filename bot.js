90da4d3cf950b6e2c5fc6a16f851173c13f452f4
eggonabull <eggonabull@hotmail.com>
2017-02-05 00:32:00 -0800

    Start adding support for breaking apart bot functionality.

--- bot.js
+++ bot.js
@@ -1,35 +1,48 @@
+#!/usr/bin/env node
+var secrets = require('./secrets');
 var mineflayer = require('mineflayer');
 var navigatePlugin = require('mineflayer-navigate')(mineflayer);
 var scaffoldPlugin = require('mineflayer-scaffold')(mineflayer);
-var bot = mineflayer.createBot({
-  host: "192.168.1.9",   // optional
-  port: 25565,         // optional
-  username: "username",
-  password: "password"
-});
-navigatePlugin(bot);
-scaffoldPlugin(bot);
-bot.on('chat', function(username, message) {
-  if (username === bot.username) return;
-  console.log("player", bot.players);
-  console.log("username", username);
+var requireIndex = require('requireindex');
+var fs = require('fs');
+var path = require('path');
 
-  message.split(" ")
-  
-  if (message === 'come') {
-    var target = bot.players[username].entity;
-    if(target === null) {
-      bot.chat("You're too far away, where are you?");
-      return;
-    }
-    bot.scaffold.to(target.position, function(err) {
-      if (err) {
-        bot.chat("didn't make it: " + err.code);
-      } else {
-        bot.chat("made it!");
+function init(argv) {
+  var bot = mineflayer.createBot({
+    host: "192.168.1.9",   // optional
+    port: 25565,         // optional
+    username: secrets.username,
+    password: secrets.password
+  });
+  bot.nick = secrets.nick;
+  bot.connected = true;
+  bot.on("end", function() {
+    bot.connected = false;
+    process.exit();
+  })
+  bot.setTimeout = function (fn, delay) {
+    setTimeout(function () {
+      if(bot.connected) {
+        fn();
       }
-    });
-  } else {
-    bot.chat(message);
+    }, delay);
   }
+  navigatePlugin(bot);
+  scaffoldPlugin(bot);
+  var plugins = requireIndex(path.join(__dirname, 'lib', 'plugins'));
+  for (plugin in plugins) {
+    if (plugins[plugin].inject != null) {
+        plugins[plugin].inject(bot);
+    } else {
+        console.log(plugin, 'has no inject function.');
+    }
+  }
+}
+
+/*
+process.on('uncaughtException', (e) => {
+  console.log(e);
 });
+*/
+
+init();
