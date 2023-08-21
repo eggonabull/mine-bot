eec3de07fa7406c6812f941532df72eea1257dc6
Drew Faubion <eggonabull@hotmail.com>
2023-08-21 16:05:56 -0700

    Get farm working consistently

--- plant-farm.js
+++ plant-farm.js
@@ -1,17 +1,19 @@
 import pathfinder_pkg from "mineflayer-pathfinder";
 import { Vec3 } from "vec3";
-import { sleep, pickUpItems } from "./shared.js";
+import { pickUpItems } from "./shared.js";
 import g from "./globals.js";
+import { getDistances } from "./shared.js";
 
 const { GoalNear } = pathfinder_pkg.goals;
 
 async function craftBonemeal() {
+  console.log("craftBonemeal");
   const bot = g.bot;
   const bonemeal_id = bot.registry.itemsByName.bone_meal.id;
   const bone_id = bot.registry.itemsByName.bone.id;
   const bone_meal_inv = bot.inventory.findInventoryItem(bonemeal_id);
-  if (!bone_meal_inv) {
-    console.log("bone_id", bone_id);
+  if (!bone_meal_inv || bone_meal_inv.count < 10) {
+    await bot.waitForTicks(2);
     const bone_inv = bot.inventory.findInventoryItem(bone_id);
     if (!bone_inv) {
       console.log("items", bot.inventory.items());
@@ -20,12 +22,18 @@ async function craftBonemeal() {
     }
     const recipe = bot.recipesFor(bonemeal_id)[0];
     await bot.craft(recipe);
+    await bot.waitForTicks(3);
+    await bot.craft(recipe);
+    await bot.waitForTicks(3);
+    await bot.craft(recipe);
+    await bot.waitForTicks(3);
     return true;
   }
   return true;
 }
 
 async function fertilizeCrop(block_vec) {
+  console.log("fertilizeCrop", block_vec);
   // await sleep(20)
   const bot = g.bot;
   const bonemeal_id = bot.registry.itemsByName.bone_meal.id;
@@ -34,49 +42,60 @@ async function fertilizeCrop(block_vec) {
     bot.chat("I'm out of bonemeal");
     return false;
   }
+  await bot.waitForTicks(1);
   const block = bot.blockAt(block_vec);
   if (block._properties.age == 7) {
     return true;
   }
-  console.log("block", block);
   await bot.equip(bone_meal_inv);
   await bot.lookAt(block_vec);
   try {
-    bot.activateBlock(block, new Vec3(0, 1, 0));
-    await sleep(50);
-    bot.activateBlock(block, new Vec3(0, 1, 0));
-    await sleep(50);
-    bot.activateBlock(block, new Vec3(0, 1, 0));
-    await sleep(50);
-    bot.activateBlock(block, new Vec3(0, 1, 0));
+    await bot.activateBlock(block, new Vec3(0, 1, 0));
+    await bot.waitForTicks(2);
+    await bot.activateBlock(block, new Vec3(0, 1, 0));
+    await bot.waitForTicks(2);
+    await bot.activateBlock(block, new Vec3(0, 1, 0));
+    await bot.waitForTicks(2);
+    await bot.activateBlock(block, new Vec3(0, 1, 0));
+    await bot.waitForTicks(2);
+    await bot.activateBlock(block, new Vec3(0, 1, 0));
   } catch (error) {
     console.log("error", error);
   }
   return true;
 }
 
+let breakFarmBlockCount = 0;
+
 async function breakFarmBlock(block_vec, crop_name, item_name) {
+  console.log("breakFarmBlock", block_vec, crop_name, item_name);
   const bot = g.bot;
   const block = bot.blockAt(block_vec);
   if (block._properties.age == 7) {
     await bot.dig(block);
-    await sleep(100);
-    await pickUpItems();
-    await bot.pathfinder.goto(
-      new GoalNear(block_vec.x, block_vec.y, block_vec.z, 2),
-    );
-    return await placeFarmBlock(block_vec, crop_name, item_name);
+    await bot.waitForTicks(1);
+    breakFarmBlockCount++;
+    if (breakFarmBlockCount % 10 == 0) {
+      console.log("start pickup")
+      await pickUpItems();
+      if (getDistances(block_vec).posdiff > 4) {
+        await bot.pathfinder.goto(
+          new GoalNear(block_vec.x, block_vec.y, block_vec.z, 2),
+        );
+      }
+    }
+  } else {
+    console.log("block._properties.age", block._properties.age);
   }
   return true;
 }
 
 async function placeFarmBlock(block_vec, crop_name, item_name) {
+  console.log("placeFarmBlock", block_vec, crop_name, item_name);
   const bot = g.bot;
-  await sleep(100);
   const item_id = bot.registry.itemsByName[item_name].id;
-  const item_inv = bot.inventory.findInventoryItem(item_id);
+  let item_inv = bot.inventory.findInventoryItem(item_id);
   const block = bot.blockAt(block_vec);
-  console.log("placeFarmBlock", block);
   const dirt_below = bot.blockAt(
     new Vec3(block_vec.x, block_vec.y - 1, block_vec.z),
   );
@@ -92,14 +111,25 @@ async function placeFarmBlock(block_vec, crop_name, item_name) {
     return true;
   }
   if (!item_inv) {
-    bot.chat(`I'm all out of ${item_name}`);
+    await pickUpItems();
+    await bot.pathfinder.goto(new GoalNear(block_vec.x, block_vec.y, block_vec.z, 2));
+    item_inv = bot.inventory.findInventoryItem(item_id);
+    if (!item_inv) {
+      bot.chat(`I'm all out of ${item_name}`);
+      g.mode = null;
+      return false;
+    }
   }
 
+  await bot.waitForTicks(1);
   await bot.lookAt(block_vec);
-  await sleep(100);
   await bot.equip(item_inv, "hand");
   try {
-    await bot.placeBlock(bot.blockAt(block_vec), new Vec3(0, 1, 0));
+    await bot.waitForTicks(10);
+    while (bot.blockAt(block_vec).name != crop_name) {
+      await bot.activateBlock(bot.blockAt(block_vec), new Vec3(0, 1, 0));
+      await bot.waitForTicks(5);
+    }
   } catch (error) {
     console.log(error);
   }
@@ -133,11 +163,10 @@ export async function farmCrops(crop_name) {
   let loop = true;
   g.mode = "farmCrops";
   while (loop && g.mode === "farmCrops") {
+    console.log("loop start")
     loop = loop && (await craftBonemeal());
-    console.log("craftBonemeal", loop);
     loop = loop && (await fertilizeCrop(block_vec));
-    console.log("fertilizeCrop", loop);
     loop = loop && (await breakFarmBlock(block_vec, crop_name, item_name));
-    console.log("breakFarmBlock", loop);
+    loop = loop && (await placeFarmBlock(block_vec, crop_name, item_name));
   }
 }
